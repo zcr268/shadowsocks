6b46061d688e652ef4d5e9c6fbdd0bfead316a6b
zcr268 <zcr268@DESKTOP-WIN>
2020-04-18 03:26:21 +0800

    shadowsocks_3.8.6.3.tar.gz 8be99a86fb3b1ebc8dcd7bde9c888f50

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=`cat shadowsocks/ss/version`
+version=`cat shadowsocks/ss/version|awk -F '.' '{for(i=1;i<=NF;i++){if(i!=NF){printf("%s.",$i)}else{printf("%s",$i+1)}}}'`
 file_name="shadowsocks_${version}.tar.gz"
 echo ${file_name}
 tar -czf ${file_name} shadowsocks
@@ -11,9 +11,11 @@ cat version_bak >> version
 rm version_bak
 cp ${file_name} shadowsocks.tar.gz
 rm -rf shadowsocks
-
-scp -P 22 shadowsocks.tar.gz version admin@192.168.50.110:/volume1/docker/nginx/home/
-
-git add .
-git commit -m "${file_name} ${md5} $1"
-git push
+echo -n "提交信息:"
+msg=`read`
+# if false; then
+    scp -P 22 shadowsocks.tar.gz version admin@192.168.50.110:/volume1/docker/nginx/home/
+    git add .
+    git commit -m "${file_name} ${md5} $msg"
+    git push
+# fi
